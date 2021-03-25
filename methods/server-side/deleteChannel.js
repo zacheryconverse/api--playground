6de28b6b62d075e9ed93a9a13b8aa6d23a58cc15
Zachery Converse <zacheryconverse@gmail.com>
2021-03-25 11:13:11 -0600

    update stream-chat

--- methods/server-side/deleteChannel.js
+++ methods/server-side/deleteChannel.js
@@ -4,7 +4,6 @@ const deleteChannel = async (channelType, channelId) => {
   const channel = serverClient.channel(channelType, channelId);
   try {
     await channel.create()
-
   } catch (err) {
     console.error(err);
   }
